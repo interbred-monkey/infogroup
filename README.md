29287723a377f08597ed8249eb3255e9136e0117
Simon Mudd <simon"scoot.co.uk>
2014-02-28 11:56:30 +0000

    Updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,138 @@
-infogroup
+InfoGroup
 =========
+
+# About
+
+A simple Node.JS wrapper around the InfoGroup business directory API
+
+## Installation
+
+[infogroup](https://npmjs.org/package/infogroup) is available through an installation from npm.
+
+```javascript
+npm install infogroup
+```
+
+## Usage
+
+To use this library simply require the file and create a new instance of the module like so:
+
+```javascript
+var InfoGroup = require('infogroup');
+var infogroup = new InfoGroup(username, password);
+```
+
+### Using Debug mode
+
+The debug mode is set to false by default, when switched on the API parameters will be logged out along with the API call that is being called and the response.
+
+```javascript
+infogroup.debugMode(true);
+```
+
+### Using Test mode
+
+Test mode is set to false by default, when switched on the module will use the InfoGroup sandbox environment to stop test data being added to the live database.
+
+```javascript
+infogroup.testMode(true);
+```
+
+### Manipulating records
+
+Parameters for adding a record are as follows:
+
+---
+
+`name` - if writing records in batches a name for the batch (require if batch writing)  
+  `batch_id` - submits records to the same batch  
+  `submissions` - one or more records to add to the database
+
+---
+
+Submission parameters are as follows:
+
+---
+
+`Submission Type` - the type of submission 
+  * A - Add record  
+  * U - Update a record  
+  * R - Renew a record  
+  * D - Delete a record  
+
+`Company Name`  
+  `Location Address`  
+  `Location City`  
+  `Location State`  
+  `Location Zip Code`  
+  `Location Phone`
+
+---
+
+Basic example:
+
+```javascript
+infogroup.getToken(function(err) {
+
+  if (err) {
+
+    console.log("Errrrrr");
+    console.log(err);
+    process.exit();
+
+  }
+
+  var params = {
+    submissions: [
+      {
+        "Submission Type": "A",
+        "Company Name": "Axel Foley",
+        "Location Address": "1154 Summit Dr",
+        "Location City": "Beverly Hills",
+        "Location State": "CA",
+        "Location Zip Code": "90210",
+        "Location Phone": "7039554747"
+      },
+      {
+        "Submission Type": "U",
+        "Company Name": "Roadhouse Diner",
+        "Location Address": "405 Memorial Drive",
+        "Location City": "Plano",
+        "Location State": "TX",
+        "Location Zip Code": "75074",
+        "Location Phone": "206-405-9181"
+      }
+    ]
+  }
+
+  / do the call for the adding data
+  infogroup.postData(params, function(err, data) {
+
+    console.log(err, data)
+
+  })
+
+})
+```
+
+Example response:
+```javascript
+{
+  "result": "success",
+  "submissions": [
+    {
+      "result": "success",
+      "id": "34c415dc-d385-11e2-8400-fda2de9a8a9c"
+    },
+    {
+      "result": "success",
+      "id": "34c66bc0-d385-11e2-946f-476095fff0ab"
+    }
+  ]
+}
+```
+
+## Third-party libraries
+
+[request](http://github.com/mikeal/request.git)  
+  [underscore](http://underscorejs.org)
---
+++ infogroup.js
@@ -0,0 +1,170 @@
+
+// include the request library
+var request = require('request');
+
+// include the underscore library
+var _ = require('underscore');
+
+var InfoGroup = function(_username, _password) {
+
+  this._username = _username;
+  this._password = _password;
+
+}
+
+InfoGroup.prototype._debug = false;
+InfoGroup.prototype._test_mode = false;
+InfoGroup.prototype._username = null;
+InfoGroup.prototype._password = null;
+InfoGroup.prototype._access_token = null;
+InfoGroup.prototype._refresh_token = null;
+InfoGroup.prototype._token_url = "https://auth.qa.data-axle.infogroup.com/oauth/token?scope="
+
+InfoGroup.prototype.getToken = function(callback) {
+
+  var _instance = this;
+
+  var params = {
+    url: this._token_url+"http://"+(this._test_mode === true?"sandbox.":"")+"bulkupdate.infogroup.com/",
+    authenticate: true
+  }
+
+  this.doRequest(params, function(err, data) {
+
+    if (!_.isNull(err)) {
+
+      return callback(err);
+
+    }
+
+    _instance._access_token = data.access_token;
+    _instance._refresh_token = data.refresh_token;
+
+    return callback(null, data);
+
+  })
+
+}
+
+// post some data
+InfoGroup.prototype.postData = function(_form_data, callback) {
+
+  if (!_.isObject(_form_data)) {
+
+    return callback("Parameters are not correctly formatted");
+
+  }
+
+  var _params = {
+    method: "POST",
+    url: "http://"+(this._test_mode === true?"sandbox.":"")+"bulkupdate.infogroup.com/api/1/submissions",
+    body: _form_data
+  }
+
+  this.doRequest(_params, function(err, data) {
+
+    return callback(err, data);
+
+  })
+
+}
+
+// make the http request
+InfoGroup.prototype.doRequest = function(_params, callback) {
+
+  // do we have a url to go to?
+  if (_.isUndefined(_params.url)) {
+
+    return callback("URL is a required parameter", null);
+
+  }
+
+  // set a return type
+  _params.json = true;
+
+  // are we requesting the token?
+  if (_params.authenticate === true) {
+
+    // remove the custom param
+    delete(_params.authenticate);
+
+    // add in the auth
+    _params.auth = {
+      user: this._username,
+      pass: this._password,
+      sendImmediately: true
+    }
+
+  }
+
+  else if (!_.isNull(this._access_token)) {
+
+    _params.headers = {
+      "X-AUTH-TOKEN": this._access_token
+    }
+
+  }
+  
+  // debug mode
+  var _debug_mode = this._debug;
+
+  // do the request
+  request(_params, function(e, r, b) {
+
+    // output some debug
+    if (_debug_mode === true) {
+
+      console.log("****************************************************");
+      console.log("** Parameters **");
+      console.log(JSON.stringify(_params, undefined, 2));
+      console.log("****************************************************");
+      console.log("** Result **");
+      console.log("Error: "+e);
+      console.log("Status: "+r.statusCode);
+      console.log("Body: "+JSON.stringify(b, undefined, 2));
+      console.log("****************************************************");
+
+    }
+
+    // Error
+    if (e) {
+
+      return callback(e, null);
+
+    }
+
+    // Not a status 200
+    if (r.statusCode !== 200) {
+
+      return callback("API call responded with a status "+r.statusCode, b);
+
+    }
+
+    // Wooooooooo Bacon
+    else {
+
+      return callback(null, b);
+
+    }
+
+  })
+
+}
+
+// turn on/off debug
+InfoGroup.prototype.setDebugMode = function(bool) {
+
+  return this._debug = Boolean(bool);
+
+}
+
+// turn on/off test mode
+InfoGroup.prototype.setTestMode = function(bool) {
+
+  return this._test_mode = Boolean(bool);
+  
+}
+
+module.exports = {
+  InfoGroup: InfoGroup
+}
